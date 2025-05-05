36bd383b4aed94f461078f77e75d888ee033cf5d
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-05 13:54:54 +0200

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Daniel A on 2025-05-05.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
